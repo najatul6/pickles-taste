04edd505e1eee49637f4707938abd2aa2a0f6132
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:33:55 +0600

    add menu in nav menu

---
+++ src/Utils/utils.js
@@ -0,0 +1,37 @@
+export const NavMenu = [
+    {
+      id: "ue8rj4_43",
+      name: "Home",
+      link: "/",
+    },
+    {
+      id: "ue7rj4_44",
+      name: "Services",
+      link: "#",
+    },
+    {
+      id: "u5erj4_45",
+      name: "Programs",
+      link: "#",
+    },
+    {
+      id: "ue76rj4_46",
+      name: "About Us",
+      link: "#",
+    },
+    {
+      id: "ue6rj4_47",
+      name: "Contact Us",
+      link: "#",
+    },
+    {
+      id: "u6erj4_48",
+      name: "Blog",
+      link: "#",
+    },
+    {
+      id: "ue78rj4_49",
+      name: "Pricing",
+      link: "#",
+    },
+  ];
