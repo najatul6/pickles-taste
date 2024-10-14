7531f0e33e1a215076a3e58228f10a3e6747d478
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 22:59:02 +0600

    designed error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,23 @@
+import errorImg from "../../assets/Error.json";
+import Lottie from "lottie-react";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="bg-[#181181A]">
+      <div className="flex flex-col min-h-screen items-center justify-center">
+        <Lottie animationData={errorImg}></Lottie>
+        <Link
+          to="/"
+          className="bg-white/5 py-4 px-5 uppercase font-bold rounded-full flex justify-center items-center gap-4 hover:bg-white/10"
+        >
+          Go to home
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
