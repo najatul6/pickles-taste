1f7363e068433ecc9fd92712b19c8b7cce4e6f44
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:22:42 +0600

    create home and about page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
 
 const Routes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     errorElement: <ErrorPage />,
-    // children: [
-    //   { path: "products", element: <ProductsList /> },
-    //   { path: "products/:productId", element: <ProductDetails /> },
-    //   { path: "cart", element: <Cart /> },
-    //   { path: "*", element: <NotFound /> },
-    // ],
+    children: [
+      { path: "/",
+         element: <Home /> 
+        },
+        {
+          path:'about',
+          element:<About/>
+        }
+    ],
   },
 ]);
 
