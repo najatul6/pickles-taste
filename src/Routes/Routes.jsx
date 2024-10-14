3433f44346327b7571a6ba419c322e9cc9149cd4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:03:16 +0600

    install lottie-react

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 export const Routes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [
-      { path: "products", element: <ProductsList /> },
-      { path: "products/:productId", element: <ProductDetails /> },
-      { path: "cart", element: <Cart /> },
-      { path: "*", element: <NotFound /> },
-    ],
+    errorElement:<ErrorPage/>,
+    // children: [
+    //   { path: "products", element: <ProductsList /> },
+    //   { path: "products/:productId", element: <ProductDetails /> },
+    //   { path: "cart", element: <Cart /> },
+    //   { path: "*", element: <NotFound /> },
+    // ],
   },
 ]);
 
