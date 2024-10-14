e4b42bed5d7e6942920b423d864215dd2bd96925
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:55:58 +0600

    footer components problem fixed

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,24 +1,24 @@
 
 const Footer = () => {
   return (
-    <footer class="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 font-sans tracking-wide">
-    <div class="py-12 px-12">
-      <div class="flex flex-wrap items-center sm:justify-between max-sm:flex-col gap-6">
+    <footer className="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 font-sans tracking-wide">
+    <div className="py-12 px-12">
+      <div className="flex flex-wrap items-center sm:justify-between max-sm:flex-col gap-6">
         <div>
-          <a href='javascript:void(0)'><img src="https://readymadeui.com/readymadeui-light.svg" alt="logo" class='w-44 ' /></a>
+          <a href='javascript:void(0)'><img src="https://readymadeui.com/readymadeui-light.svg" alt="logo" className='w-44 ' /></a>
         </div>
 
-        <ul class="flex items-center justify-center flex-wrap gap-y-2 md:justify-end space-x-6">
-          <li><a href="javascript:void(0)" class="text-gray-300 hover:underline text-base">Home</a></li>
-          <li><a href="javascript:void(0)" class="text-gray-300 hover:underline text-base">About</a></li>
-          <li><a href="javascript:void(0)" class="text-gray-300 hover:underline text-base">Services</a></li>
-          <li><a href="javascript:void(0)" class="text-gray-300 hover:underline text-base">Contact</a></li>
+        <ul className="flex items-center justify-center flex-wrap gap-y-2 md:justify-end space-x-6">
+          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Home</a></li>
+          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">About</a></li>
+          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Services</a></li>
+          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Contact</a></li>
         </ul>
       </div>
 
-      <hr class="my-6 border-gray-500" />
+      <hr className="my-6 border-gray-500" />
 
-      <p class='text-center text-gray-300 text-base'>© ReadymadeUI. All rights reserved.</p>
+      <p className='text-center text-gray-300 text-base'>© ReadymadeUI. All rights reserved.</p>
     </div>
   </footer>
   )
