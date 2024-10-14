1f5c0cf23ca2b7e57a82509cd13c2654de90574b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 22:52:56 +0600

    create routes for the given route name

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom"
+
+const Routes = createBrowserRouter([
+    {
+        path: "/",
+        element:"Home",
+    }
+])
+
+export default Routes
