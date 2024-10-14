8f58a71f70bee46ba2f0a7b5afa50079276c91e2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:53:22 +0600

    add footer in home page

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
@@ -7,6 +8,7 @@ const MainLayout = () => {
       <Navbar />
 
       <Outlet />
+      <Footer />
     </div>
   );
 };
