865e868540d885a4ed161e1a65f34258471e9367
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-14 23:58:36 +0600

    modify footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 font-sans tracking-wide">
+    <footer className="shadow-inner tracking-wide">
     <div className="py-12 px-12">
       <div className="flex flex-wrap items-center sm:justify-between max-sm:flex-col gap-6">
         <div>
@@ -9,16 +10,16 @@ const Footer = () => {
         </div>
 
         <ul className="flex items-center justify-center flex-wrap gap-y-2 md:justify-end space-x-6">
-          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Home</a></li>
-          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">About</a></li>
-          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Services</a></li>
-          <li><a href="javascript:void(0)" className="text-gray-300 hover:underline text-base">Contact</a></li>
+          <li><a href="javascript:void(0)" className=" hover:underline text-base">Home</a></li>
+          <li><a href="javascript:void(0)" className=" hover:underline text-base">About</a></li>
+          <li><a href="javascript:void(0)" className=" hover:underline text-base">Services</a></li>
+          <li><a href="javascript:void(0)" className=" hover:underline text-base">Contact</a></li>
         </ul>
       </div>
 
-      <hr className="my-6 border-gray-500" />
+      <hr className="my-6 " />
 
-      <p className='text-center text-gray-300 text-base'>© ReadymadeUI. All rights reserved.</p>
+      <p className='text-center  text-base'>&copy; <Link className="font-bold underline" to="htttps://najatul-islam.vercel.app">Najatul Islam</Link>. All rights reserved.</p>
     </div>
   </footer>
   )
