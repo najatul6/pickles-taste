55d05d6c5ea051200a883fac71f13bdfa47add72
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-15 22:48:00 +0600

    update banners

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,6 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import banner1 from "../../assets/banner/1.jfif";
-import banner2 from "../../assets/banner/2.jfif";
-import banner3 from "../../assets/banner/3.jfif";
-import banner4 from "../../assets/banner/4.jfif";
-import banner5 from "../../assets/banner/5.jfif";
-import banner6 from "../../assets/banner/6.jfif";
-import banner7 from "../../assets/banner/7.jfif";
-import banner8 from "../../assets/banner/8.jfif";
-import banner9 from "../../assets/banner/9.jfif";
+import banner1 from "../../assets/banner/01.png";
 const Banner = () => {
   return (
     <Carousel>
@@ -16,29 +8,21 @@ const Banner = () => {
         <img src={banner1} />
       </div>
       <div>
-        <img src={banner2} />
+        <img src={banner1} />
       </div>
       <div>
-        <img src={banner3} />
+        <img src={banner1} />
       </div>
       <div>
-        <img src={banner4} />
+        <img src={banner1} />
       </div>
       <div>
-        <img src={banner5} />
+        <img src={banner1} />
       </div>
       <div>
-        <img src={banner6} />
+        <img src={banner1} />
       </div>
-      <div>
-        <img src={banner7} />
-      </div>
-      <div>
-        <img src={banner8} />
-      </div>
-      <div>
-        <img src={banner9} />
-      </div>
+   
     </Carousel>
   );
 };
