b8adac836f424989d3af5edc50eedeaccc7f9f72
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-15 23:46:52 +0600

    add category in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner/Banner"
+import Category from "../../components/Category/Category"
 
 const Home = () => {
   return (
     <div className="min-h-screen">
       <Banner/>
+      <Category/>
     </div>
   )
 }
