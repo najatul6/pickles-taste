19cc7ff6a0a33976d681caefc226016fd36a3464
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-16 19:46:08 +0600

    add reviews ratings

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,11 +1,57 @@
-import SectionHeading from "../Shared/SectionHeading/SectionHeading"
+import { useEffect, useState } from "react";
+
+import SectionHeading from "../Shared/SectionHeading/SectionHeading";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Testimonial = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("/reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
   return (
     <section>
-        <SectionHeading subTitle="What Our Clients Say" title="TESTIMONIALS"/>
+      <SectionHeading subTitle="What Our Clients Say" title="TESTIMONIALS" />
+      <div>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          {reviews?.map((review) => (
+            <SwiperSlide key={review?._id}>
+              <div>
+                <Rating
+                  style={{ maxWidth: 250 }}
+                  readOnly
+                  value={review?.rating}
+                />
+                <FaQuoteLeft />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
