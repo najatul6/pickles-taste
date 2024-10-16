10224336904b5de5aee6cdaee7a53d29284cbfc6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-16 18:41:38 +0600

    create card for recommend section

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -16,7 +16,7 @@ const Recommended = () => {
   return (
     <div>
         <SectionHeading subTitle="Should Try" title="Our Recommends"/>
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {
                 offered?.map(items=><ItemCard key={items?._id} item={items}/>)
             }
--- src/components/Shared/ItemCard/ItemCard.jsx
+++ src/components/Shared/ItemCard/ItemCard.jsx
@@ -1,16 +1,16 @@
 const ItemCard = ({item}) => {
-    const
+    const {name,recipe,image,price}=item
   return (
     <div className="card rounded-none shadow-xl">
       <figure>
         <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
+          src={image}
+          alt={name}
         />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
         <div className="card-actions">
           <button className="btn btn-primary">Buy Now</button>
         </div>
