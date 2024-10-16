36d94ca73578602609a620104975ebacb0e59907
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 05:17:50 +0600

    add helmet provider for use full project

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import Routes from './Routes/Routes.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import Routes from "./Routes/Routes.jsx";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className='max-w-screen-7xl mx-auto'>
-    <RouterProvider router={Routes}/>
-    </div>
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <div className="max-w-screen-7xl mx-auto">
+        <RouterProvider router={Routes} />
+      </div>
+    </HelmetProvider>
+  </StrictMode>
+);
