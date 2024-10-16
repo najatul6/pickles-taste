7c8da387bee6d60ce1897a5999206439b1e294cb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 05:04:09 +0600

    Create all page and set routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
+import DashBoard from "../Pages/DashBoard/DashBoard";
+import OurMenu from "../Pages/OurMenu/OurMenu";
+import ContactUs from "../Pages/ContactUs/ContactUs";
+import OurShop from "../Pages/OurShop/OurShop";
+import LogIn from "../Pages/LogIn/LogIn";
+import Register from "../Pages/Register/Register";
 
 const Routes = createBrowserRouter([
   {
@@ -9,27 +15,33 @@ const Routes = createBrowserRouter([
     element: <MainLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/",
-         element: <Home /> 
-        },
-        {
-          path:'dashboard',
-          element:<DashBoard/>
-        },
-        {
-path:'our-menu',
-element:<OurMenu/>,
-        },
-        {
-          path:'contact-us',
-          element:<Contact/>,
-        },
-        {
-          path:'our-shop',
-          element:<OurShop/>,
-        },
+      { path: "/", element: <Home /> },
+      {
+        path: "dashboard",
+        element: <DashBoard />,
+      },
+      {
+        path: "our-menu",
+        element: <OurMenu />,
+      },
+      {
+        path: "contact-us",
+        element: <ContactUs />,
+      },
+      {
+        path: "our-shop",
+        element: <OurShop />,
+      },
     ],
   },
+  {
+    path:"/login",
+    element:<LogIn/>
+  },
+  {
+    path:"/register",
+    element:<Register/>
+  }
 ]);
 
 export default Routes;
