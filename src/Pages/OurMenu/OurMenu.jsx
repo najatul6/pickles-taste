e740e8ed8d4651f8b8f1d59ef7ac399457dd7796
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 23:40:37 +0600

    DESSERTS Item

--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -3,10 +3,18 @@ import SectionBanner from "../../components/Shared/SectionBanner/SectionBanner";
 import menuBack from "../../assets/banner/3.jfif";
 import useMenu from "../../hooks/useMenu";
 import SectionHeading from "../../components/Shared/SectionHeading/SectionHeading";
+import ItemsLoading from "../../components/Shared/ItemsLoading/ItemsLoading";
+import MenuItemCard from "../../components/Shared/MenuItemCard/MenuItemCard";
+import MenuBtn from "../../components/Shared/MenuBtn/MenuBtn";
 
 const OurMenu = () => {
   const [menu, loading] = useMenu();
-  const popular = menu?.filter((item) => item.category === "popular");
+  const offered = menu?.filter((item) => item.category === "offered");
+  const dessert = menu?.filter((item) => item.category === "dessert");
+  const pizza = menu?.filter((item) => item.category === "pizza");
+  const salad = menu?.filter((item) => item.category === "salad");
+  const soup = menu?.filter((item) => item.category === "soup");
+
   return (
     <div className="min-h-screen">
       {/* React Helmet  */}
@@ -20,16 +28,49 @@ const OurMenu = () => {
         description="Would you like to try a dish?"
       />
       {/* Page content */}
-      <section>
-      {/* Todays Offer */}
-      <div>
-        <SectionHeading subTitle="Don't miss" title="TODAY'S OFFER"/>
-        {/* TODAY'S OFFER Items */}
-        <div>
-          
+      <section className="space-y-5">
+        {/* Todays Offer */}
+        <div className="w-11/12 mx-auto">
+          <SectionHeading subTitle="Don't miss" title="TODAY'S OFFER" />
+          {/* TODAY'S OFFER Items */}
+          <div>
+            {/* Show loading and menu item list  */}
+            {loading ? (
+              <ItemsLoading />
+            ) : (
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {offered?.map((item) => (
+                  <MenuItemCard loading={loading} key={item?._id} item={item} />
+                ))}
+              </div>
+            )}
+            {/* Menu button */}
+            <MenuBtn text="ORDER YOUR favourite FOOD" />
+          </div>
         </div>
-      </div>
-
+        {/* DESSERTS Item */}
+        <div className="w-11/12 mx-auto">
+          <SectionBanner
+            bgImg={menuBack}
+            description="Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            title="DESSERTS"
+          />
+          {/* DESSERTS OFFER Items */}
+          <div>
+            {/* Show loading and menu item list  */}
+            {loading ? (
+              <ItemsLoading />
+            ) : (
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {dessert?.map((item) => (
+                  <MenuItemCard loading={loading} key={item?._id} item={item} />
+                ))}
+              </div>
+            )}
+            {/* Menu button */}
+            <MenuBtn text="ORDER YOUR favourite FOOD" />
+          </div>
+        </div>
       </section>
     </div>
   );
