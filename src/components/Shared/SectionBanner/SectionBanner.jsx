3a73c263d8307c61ff98966aa8f21156d5da63ff
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 06:17:54 +0600

    update our menu page

--- src/components/Shared/SectionBanner/SectionBanner.jsx
+++ src/components/Shared/SectionBanner/SectionBanner.jsx
@@ -1,19 +1,25 @@
 import { Parallax } from "react-parallax";
 import PropTypes from "prop-types";
-const SectionBanner = ({ bgImg, title }) => {
+const SectionBanner = ({ bgImg, title ,description}) => {
   return (
     <Parallax
       blur={{ min: -50, max: 50 }}
       bgImage={bgImg}
       bgImageAlt={title}
-      strength={-200}
+      strength={-300}
     >
-      <div className="border w-3/4 my-24 bg-black/40 mx-auto h-96 text-white flex flex-col items-center justify-center">
+      <div className="border w-3/4 my-24 bg-black/40 mx-auto h-60 text-white flex flex-col items-center justify-center">
         <h1 className="text-2xl lg:text-4xl uppercase">{title}</h1>
         <p>{description}</p>
       </div>
     </Parallax>
   );
+};
+
+SectionBanner.propTypes = {
+    bgImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default SectionBanner;
