87de82463a2e20463c0688a8f8aa90b863f11836
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 21:35:05 +0600

    update banner section

--- src/components/Shared/SectionBanner/SectionBanner.jsx
+++ src/components/Shared/SectionBanner/SectionBanner.jsx
@@ -9,7 +9,7 @@ const SectionBanner = ({ bgImg, title ,description}) => {
       strength={-300}
     >
       <div className="md:w-3/4 py-5 my-5 md:my-24 bg-black/70 mx-auto h-40 lg:h-60 text-center text-white flex flex-col items-center justify-center">
-        <h1 className="text-xl font-bold lg:text-4xl uppercase">{title}</h1>
+        <h1 className="text-xl font-bold lg:text-4xl uppercase mb-5">{title}</h1>
         <p className="font-thin md:text-xl">{description}</p>
       </div>
     </Parallax>
