c02c3cce5c16617510f49cea9b59390846c20c8a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-18 00:42:15 +0600

    install  react-tabs

--- src/Pages/OurShop/OurShop.jsx
+++ src/Pages/OurShop/OurShop.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async"
+import SectionBanner from "../../components/Shared/SectionBanner/SectionBanner"
+import bgImage from "../../assets/banner/8.jfif"
 
 const OurShop = () => {
   return (
@@ -7,7 +9,9 @@ const OurShop = () => {
         <title>Our Shop | Pickles Taste</title>
         <meta name="description" content="Our Shop Page" />
       </Helmet>
-      OurShop
+      
+      {/* Shop Banner  */}
+      <SectionBanner bgImg={bgImage} title="our shop" description="Would you like to try a dish?"/>
       </div>
   )
 }
