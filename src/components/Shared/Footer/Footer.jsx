c6d5827f14e85ee5125751fb8ba2568b501d1511
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-18 02:10:46 +0600

    update footer options

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,29 +1,77 @@
-import { Link } from "react-router-dom"
-import logo from "../../../assets/logo.svg"
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/logo.svg";
 
 const Footer = () => {
   return (
     <footer className="shadow-2xl bg-white/5 text-white tracking-wide">
-    <div className="py-12 px-12">
-      <div className="flex flex-wrap items-center sm:justify-between max-sm:flex-col gap-6">
-        <div>
-          <Link to="/"><img src={logo} alt="logo" className='w-44 ' /></Link>
+      <div className="py-12 px-12">
+        <div className="flex flex-wrap items-center sm:justify-between max-sm:flex-col gap-6">
+          <div>
+            <Link to="/">
+              <img src={logo} alt="logo" className="w-44 " />
+            </Link>
+          </div>
+
+          <ul className="flex items-center justify-center flex-wrap gap-y-2 md:justify-end space-x-6">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-brand-color text-base" : "hover:underline text-base"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/our-menu"
+                className={({ isActive }) =>
+                  isActive ? "text-brand-color text-base" : "hover:underline text-base"
+                }
+              >
+                Our Menu
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/our-shop"
+                className={({ isActive }) =>
+                  isActive ? "text-brand-color text-base" : "hover:underline text-base"
+                }
+              >
+                Our Shop
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact-us"
+                className={({ isActive }) =>
+                  isActive ? "text-brand-color text-base" : "hover:underline text-base"
+                }
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
         </div>
 
-        <ul className="flex items-center justify-center flex-wrap gap-y-2 md:justify-end space-x-6">
-          <li><Link to="/" className=" hover:underline text-base">Home</Link></li>
-          <li><Link to="/our-menu" className=" hover:underline text-base">Our Menu</Link></li>
-          <li><Link to="/our-shop" className=" hover:underline text-base">Our Shop</Link></li>
-          <li><Link to="/contact-us" className=" hover:underline text-base">Contact Us</Link></li>
-        </ul>
+        <hr className="my-6" />
+
+        <p className="text-center  text-base">
+          &copy;{" "}
+          <Link
+            className="font-bold underline"
+            to="https://najatul-islam.vercel.app/"
+            target="_blank"
+          >
+            Najatul Islam
+          </Link>
+          . All rights reserved.
+        </p>
       </div>
+    </footer>
+  );
+};
 
-      <hr className="my-6" />
-
-      <p className='text-center  text-base'>&copy; <Link className="font-bold underline" to="https://najatul-islam.vercel.app/" target="_blank">Najatul Islam</Link>. All rights reserved.</p>
-    </div>
-  </footer>
-  )
-}
-
-export default Footer
+export default Footer;
