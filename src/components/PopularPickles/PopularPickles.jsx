e25a4137b48e9205c233b85218e48d79694d307d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-17 23:05:38 +0600

    update loading effect

--- src/components/PopularPickles/PopularPickles.jsx
+++ src/components/PopularPickles/PopularPickles.jsx
@@ -4,36 +4,33 @@ import MenuBtn from "../Shared/MenuBtn/MenuBtn";
 import useMenu from "../../hooks/useMenu";
 
 const PopularPickles = () => {
-  const [menu,loading] = useMenu();
-  const popular=menu?.filter((item) => item.category === "popular")
-  
-  // Loading State 
+  const [menu, loading] = useMenu();
+  const popular = menu?.filter((item) => item.category === "popular");
+
+  // Loading State
   if (loading) {
     return (
       <section className="w-11/12 mx-auto">
-        <SectionHeading title="Popular Items" subTitle="Check it out" />
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          {/* Display a loading skeleton for the list */}
-          {[...Array(4)].map((_, index) => (
-            <div key={index} className="flex w-52 flex-col gap-4">
-              <div className="flex items-center gap-4">
-                <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-                <div className="flex flex-col gap-4">
-                  <div className="skeleton h-4 w-20"></div>
-                  <div className="skeleton h-4 w-28"></div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {/*  Display a loading skeleton for the list  */}
+            {[...Array(4)].map((_, index) => (
+                <div key={index} className="flex items-center gap-4">
+                  <div className="skeleton rounded-e-full rounded-es-full w-[118px] mx-auto  h-[104px]"></div>
+                  <div className="flex flex-col gap-4 w-full">
+                    <div className="skeleton h-4 "></div>
+                    <div className="skeleton h-4 "></div>
+                  </div>
                 </div>
-              </div>
-              <div className="skeleton h-32 w-full"></div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
     );
   }
 
   return (
     <section className="w-11/12 mx-auto">
       <SectionHeading title="Popular Items" subTitle="Check it out" />
+      {loading? }
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {popular?.map((item) => (
           <MenuItemCard loading={loading} key={item?._id} item={item} />
