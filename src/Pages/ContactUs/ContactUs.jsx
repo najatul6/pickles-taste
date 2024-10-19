68edbe7225cadabe91f4998016dea59df19db480
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-19 09:15:28 +0600

    Complete Information section

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -19,36 +19,38 @@ const ContactUs = () => {
       />
       <div>
         <SectionHeading title="OUR LOCATION" subTitle="Visit Us" />
-        <div className="w-11/12 grid grid-cols-1 md:grid-cols-3 gap-6 place-items-center">
-          <div className="flex flex-col justify-center items-center text-white">
-            <div className="bg-brand-color/90">
+        {/* Information section  */}
+        <section className="w-11/12 grid grid-cols-1 md:grid-cols-3 gap-6 place-items-center mx-auto mb-10">
+          <div className="flex flex-col justify-center items-center text-white w-full">
+            <div className="bg-brand-color/90 w-full flex justify-center items-center py-5">
               <FaPhone size={"32px"} />
             </div>
-            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5">
+            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5 gap-4 py-10  h-[200px]">
               <h2 className="text-2xl font-bold">Phone</h2>
-              <p>+1 (123) 456-7890</p>
+              <p>+88 01773 827-414</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center text-white">
-            <div className="bg-brand-color/90">
+          <div className="flex flex-col justify-center items-center text-white w-full">
+            <div className="bg-brand-color/90 w-full flex justify-center items-center py-5">
               <FaMapLocation size={"32px"} />
             </div>
-            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5">
+            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5 gap-4 py-10  h-[200px]">
               <h2 className="text-2xl font-bold">Address</h2>
-              <p>123 Main St, Anytown, USA</p>
+              <p>Rangpur City, Rangpur</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center text-white">
-            <div className="bg-brand-color/90">
+          <div className="flex flex-col justify-center items-center text-white w-full">
+            <div className="bg-brand-color/90 w-full flex justify-center items-center py-5">
               <FaClock size={"32px"} />
             </div>
-            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5">
+            <div className="flex flex-col justify-center items-center w-3/4 bg-white/5 gap-4 py-10  h-[200px]">
               <h2 className="text-2xl font-bold">WORKING HOURS</h2>
               <p>Mon - Fri: 08:00 - 22:00</p>
               <p>Sat - Sun: 10:00 - 23:00</p>
             </div>
           </div>
-        </div>
+        </section>
+
       </div>
     </section>
   );
