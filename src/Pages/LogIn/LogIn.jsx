a8573cb5aba4c8520c2c2047653f010d0d996cba
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-20 04:05:57 +0600

    update inputs

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
-import bannerImg from "../../assets/banner/5.jfif"
+import bannerImg from "../../assets/banner/5.jfif";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const LogIn = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const [isShow, setIsShow] = useState(true);
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <Helmet>
@@ -19,57 +26,70 @@ const LogIn = () => {
               If you already a member, easily log in now.
             </p>
 
-            <form onSubmit="" className="flex flex-col gap-4">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
               {/* Email input field  */}
-              <input
-                className="p-2 mt-8 rounded-xl border"
-                type="email"
-                name="email"
-                placeholder="Enter Your Email"
-              />
-              {/* Password input  */}
-              <div className="relative">
+              <div>
                 <input
-                  className="p-2 rounded-xl border w-full"
-                  type={isShow ? "password" : "text"}
-                  name="password"
-                  id="password"
-                  placeholder="Enter Your Password"
+                  className="p-2 mt-8 rounded-xl border"
+                  {...register("email", { required: true })}
+                  aria-invalid={errors.email ? "true" : "false"}
+                  placeholder="Enter Your Email"
                 />
-                {/* Eye Button  */}
-                <button
-                  type="button"
-                  onClick={() => setIsShow(!isShow)}
-                  className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer z-20 opacity-100"
-                >
-                  {isShow ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="gray"
-                      id="togglePassword"
-                      className="bi bi-eye "
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"></path>
-                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"></path>
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-eye-slash-fill "
-                      
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"></path>
-                      <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"></path>
-                    </svg>
-                  )}
-                </button>
+                {errors.email?.type === "required" && (
+                  <p role="alert">Email is required</p>
+                )}
+              </div>
+              {/* Password input  */}
+              <div>
+                <div className="relative">
+                  <input
+                    className="p-2 rounded-xl border w-full"
+                    type={isShow ? "password" : "text"}
+                    name="password"
+                    {...register("password", { required: true })}
+                    aria-invalid={errors.password ? "true" : "false"}
+                    placeholder="Enter Your Password"
+                  />
+                  {/* Eye Button  */}
+                  <button
+                    type="button"
+                    onClick={() => setIsShow(!isShow)}
+                    className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer z-20 opacity-100"
+                  >
+                    {isShow ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="gray"
+                        id="togglePassword"
+                        className="bi bi-eye "
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"></path>
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-eye-slash-fill "
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"></path>
+                        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </div>
+                {errors.password?.type === "required" && (
+                  <p role="alert">Password is required</p>
+                )}
               </div>
 
               {/* Log In button  */}
@@ -121,15 +141,15 @@ const LogIn = () => {
               Forget password?
             </div>
 
-          {/* Register Link  */}
+            {/* Register Link  */}
             <div className="mt-4 text-sm flex justify-between items-center container-mr">
               <p className="mr-3 md:mr-0 ">
                 If you don&apos;t have an account..
               </p>
               <Link to="/register">
-              <button className="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">
-                Register
-              </button>
+                <button className="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">
+                  Register
+                </button>
               </Link>
             </div>
           </div>
