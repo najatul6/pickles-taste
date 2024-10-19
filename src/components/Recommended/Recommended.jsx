69846c92795784731e63e129b71ecb50e39d0b8e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-19 23:36:19 +0600

    fix update problem

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,17 +1,18 @@
 import SectionHeading from "../Shared/SectionHeading/SectionHeading"
 import useMenu from "../../hooks/useMenu"
-import ShopCategroy from "../Shared/ShopCategory/ShopCategory"
+import ShopCategory from "../Shared/ShopCategory/ShopCategory"
 import CardLoading from "../Shared/cardLoading/CardLoading"
 
 const Recommended = () => {
-    const [menu] = useMenu()
+    const [menu,loading] = useMenu()
     const offeredItem = menu?.filter(item=> item.category === 'offered')
    
   return (
     <section className="w-11/12 mx-auto">
         <SectionHeading subTitle="Should Try" title="Our Recommends"/>
-        <ShopCategroy items={offeredItem}/>
-       <CardLoading/>
+        {loading?<CardLoading/>:
+        <ShopCategory items={offeredItem}/>
+        }
     </section>
   )
 }
