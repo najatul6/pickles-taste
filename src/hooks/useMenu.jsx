f7a4fceff0d51186eae02301974fb4b6bb04f807
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-19 23:41:57 +0600

    fix to get full data

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 
 const useMenu = () => {
-    const [menu,setMenu]=useState([])
-    const [loading,setLoading]=useState(true)
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/pickles')
-       .then(res=>res.json())
-       .then(data=>{
-            setMenu(data)
-            setLoading(false)
-        })
-    },[])
-  return [menu,loading]
-}
+  useEffect(() => {
+    fetch("http://localhost:5000/pickles")
+      .then((res) => res.json())
+      .then((data) => {
+        setMenu(data);
+        setLoading(false);
+      });
+  }, []);
+  return [menu, loading];
+};
 
-export default useMenu
+export default useMenu;
