bc23ad631aef7119b3e1fa71f0c4aa82b3091475
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-19 08:53:07 +0600

    Add SectionHeading in contact page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,17 +1,20 @@
 import { Helmet } from "react-helmet-async"
 import SectionBanner from "../../components/Shared/SectionBanner/SectionBanner"
 import bannerImg from "../../assets/banner/6.jfif"
+import SectionHeading from "../../components/Shared/SectionHeading/SectionHeading"
 
 const ContactUs = () => {
   return (
-    <div>
+    <section>
       <Helmet>
         <title>Contact Us | Pickles Taste</title>
         <meta name="description" content="Contact Us Page of Asar Market" />
       </Helmet>
       <SectionBanner bgImg={bannerImg} title="Contact Us" description="Would you like to try a dish?"/>
-      
+      <div>
+        <SectionHeading title="OUR LOCATION" subTitle="Visit Us"/>
       </div>
+      </section>
   )
 }
 
