315f028e5378520d346fc89358cf54229def65cb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-20 06:06:04 +0600

    Set current user and check user is assigned

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,27 @@
 import PropTypes from "prop-types";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/firebase.config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
   const authInfo = {
+    loading,
     user,
   };
-  console.log("user:",user);
+  console.log("user:", user);
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
