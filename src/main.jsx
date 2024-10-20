677864961ff765f5989200bfb842b1945e8f83c0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-20 06:36:23 +0600

    wrap react tostify for all component

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,20 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
-import "./index.css";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div className="max-w-screen-7xl mx-auto">
-          <RouterProvider router={Routes} />
-        </div>
+        <ToastContainer>
+          <div className="max-w-screen-7xl mx-auto">
+            <RouterProvider router={Routes} />
+          </div>
+        </ToastContainer>
       </HelmetProvider>
     </AuthProvider>
   </StrictMode>
