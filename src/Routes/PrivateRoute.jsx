e669a876e31b0a31e51a43c9465c2500ff37a920
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-21 04:45:25 +0600

    add new loading effect

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -2,10 +2,14 @@ import { useContext } from "react"
 import { AuthContext } from "../providers/AuthProvider"
 import { Navigate, useLocation } from "react-router-dom"
 import PropTypes from "prop-types"
+import LoadingEffect from "../components/Shared/LoadingEffect/LoadingEffect"
 
 const PrivateRoute = ({children}) => {
-    const {user} =useContext(AuthContext)
+    const {user,loading} =useContext(AuthContext)
     const location=useLocation()
+    if(loading){
+        return <LoadingEffect/>
+    }
     if(user){
         return children
     }
---
+++ src/components/Shared/LoadingEffect/LoadingEffect.jsx
@@ -0,0 +1,17 @@
+// import styled from "styled-components";
+
+const LoadingEffect = () => {
+  return (
+    <div className="min-h-screen dark:bg-slate-800 gap-6 flex items-center justify-center">
+    <div className="w-36 h-36 border-8 border-dashed rounded-full border-t-lime-400 animate-spin">
+      
+    </div>
+    <div className="w-36 h-36 border-8 rounded-full border-t-lime-400 animate-spin">
+    </div>
+  </div>
+  );
+};
+
+
+
+export default LoadingEffect;
