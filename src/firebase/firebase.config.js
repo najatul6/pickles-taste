70a221ff7f82b1d13554443ed6c549a281da4d3f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-20 07:10:59 +0600

    comment some unused data

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,4 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
