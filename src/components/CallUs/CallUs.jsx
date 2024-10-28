ffd6f388210bc182f8d04f43509efedcb4bb302e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-28 22:22:01 +0600

    update call section

--- src/components/CallUs/CallUs.jsx
+++ src/components/CallUs/CallUs.jsx
@@ -2,7 +2,7 @@
 const CallUs = () => {
   return (
     <section className="bg-white/5 py-10 md:py-24 w-11/12 mx-auto text-white text-center">
-        <p className="text-2xl lg:text-5xl font-semibold font-Raleway">Call Us: +88 019383822737</p>
+        <p className="text-2xl lg:text-5xl font-semibold font-Raleway">Call Us: +88 01938322737</p>
     </section>
   )
 }
