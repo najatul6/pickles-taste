a530aa34353a9b9bb80363e0649f4e326d422852
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-28 23:44:14 +0600

    update route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -13,7 +13,7 @@ const PrivateRoute = ({children}) => {
     if(user){
         return children
     }
-  return <Navigate to="/login" state={{from:location}}/>
+  return <Navigate to="/login" state={{from:location}} replace/>
 }
 
 PrivateRoute.propTypes={
