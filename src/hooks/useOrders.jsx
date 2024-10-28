0d80967a85da069a32182f22b29638d8beccc229
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-29 00:36:05 +0600

    create new hook for get all orders

--- src/hooks/useOrders.jsx
+++ src/hooks/useOrders.jsx
@@ -1,8 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosSecure from "./useAxiosSecure"
 
 const useOrders = () => {
-  return (
-    <div>useOrders</div>
-  )
+    const axiosSecure=useAxiosSecure()
+  const {data:orders=[]}=useQuery({
+    queryKey:["orders"],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/orders')
+      return res.data
+    }
+  })
+  return [orders]
 }
 
 export default useOrders
