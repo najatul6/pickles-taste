333710cd6dbc7674915cc326fbb8f2978f3aba4c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-29 22:46:18 +0600

    update some secret keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain:import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket:import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_CONSOLE,
+  authDomain:import.meta.env.VITE_FIREBASE_DOMAIN_CONSOLE,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID_CONSOLE,
+  storageBucket:import.meta.env.VITE_FIREBASE_STORAGEBUCKET_CONSOLE,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID_CONSOLE,
+  appId: import.meta.env.VITE_FIREBASE_APPID_CONSOLE,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID_CONSOLE,
 };
 
 // Initialize Firebase
