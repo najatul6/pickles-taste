65d37757a30148bd705adc31ecedf6001f484e45
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-20 23:38:50 +0600

    update Cart component to use full screen height for improved visibility

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 
 const Cart = ({openCart}) => {
     return (
-        <div className={`${openCart?"block":"hidden"} w-48 bg-black absolute top-0 bottom-0 left-0`}>
+        <div className={`${openCart?"block":"hidden"} w-48 h-screen bg-black absolute top-0 bottom-0 left-0`}>
             <h2>Your Cart</h2>
             <p>No items in your cart.</p>
         </div>
