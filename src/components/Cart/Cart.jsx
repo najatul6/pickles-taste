9c0787073a614e8d30cfda2238730353bac312d4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-20 23:28:47 +0600

    update Cart component styling for improved layout and visibility

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 
 const Cart = () => {
     return (
-        <div>
+        <div className="w-48 bg-black absolute top-0 bottom-0 left-0">
             <h2>Your Cart</h2>
             <p>No items in your cart.</p>
         </div>
