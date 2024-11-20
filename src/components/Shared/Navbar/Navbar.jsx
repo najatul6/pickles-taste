19aa113724a30b620a7eee57466aa4340135a775
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-20 23:25:33 +0600

    update Navbar styling for improved visibility

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="container relative flex justify-between items-center py-2  backdrop-blur-sm">
+        <div className="container relative flex justify-between items-center py-2  backdrop-blur-md bg-black/10">
           {/* Mobile hamburger Menu Section  */}
           <div className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? (
