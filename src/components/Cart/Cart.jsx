91ea3d1a9f413be13b7a875afb7bc5039280ca3b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-28 23:17:20 +0600

    enhance Cart component with icon and header for improved user experience

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import { FaArrowRight } from "react-icons/fa";
+
 
 const Cart = ({ openCart, setOpenCart }) => {
   return (
@@ -8,12 +10,15 @@ const Cart = ({ openCart, setOpenCart }) => {
       } w-56 border duration-300 h-screen bg-darkness absolute top-0 bottom-0 right-0`}
     >
       <div className="flex flex-col justify-between h-full pb-1 border">
-        <button
-          onClick={() => setOpenCart(false)}
-          className="w-full py-1 border"
-        >
-          Close
-        </button>
+        <div>
+          <h2>My Cart</h2>
+          <button
+            onClick={() => setOpenCart(false)}
+            className="w-full py-1 border"
+          >
+           <FaArrowRight />
+          </button>
+        </div>
         <p>No items in your cart.</p>
         <button className="w-full py-2 border">Checkout</button>
       </div>
