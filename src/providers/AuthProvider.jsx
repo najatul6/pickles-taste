1a2d060cd22bd3fdf830314570218f0525149519
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-28 22:11:30 +0600

    console user emails

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -76,7 +76,7 @@ const AuthProvider = ({ children }) => {
     resetPassword,
     updateUserProfile
   };
-  console.log("user:", user);
+  console.log("user:", user?.email);
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
