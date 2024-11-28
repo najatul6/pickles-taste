4fad00aace83caac8aeb6499a90ab7101243c8ae
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-28 22:28:03 +0600

    refactor Cart component to adjust width and add checkout button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,15 +6,19 @@ const Cart = ({ openCart, setOpenCart }) => {
         <div
             className={`${
                 openCart ? "translate-x-0" : "translate-x-full"
-            } w-48 duration-300 h-screen bg-darkness absolute top-[77px] bottom-0 right-0`}
+            } w-56 border duration-300 h-screen bg-darkness absolute top-[77px] bottom-0 right-0`}
         >
+            <div className="flex flex-col justify-between h-3/4 md:h-[89%] border">
+
             <button
                 onClick={() => setOpenCart(false)}
                 className="w-full py-1 border"
-            >
+                >
                 Close
             </button>
             <p>No items in your cart.</p>
+            <button className="w-full py-2 border">Checkout</button>
+                </div>
         </div>
     );
 };
