8796b8f8aee73961b7ef460e06b9b662f8fc625a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-28 23:50:53 +0600

    remove unnecessary Swal.fire notification on login redirection

--- src/components/Shared/ItemCard/ItemCard.jsx
+++ src/components/Shared/ItemCard/ItemCard.jsx
@@ -48,11 +48,7 @@ const ItemCard = ({ item }) => {
       }).then((result) => {
         if (result.isConfirmed) {
           navigate("/login", { state: { from: location } });
-          Swal.fire({
-            title: "Log in here!",
-            text: "Provide your data to go to log in!",
-            icon: "success",
-          });
+          
         }
       });
     }
