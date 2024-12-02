983b6775e450082f4668ff2fddf59a63ab08f298
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-02 23:46:03 +0600

    feat(hooks): return axios instance for public API requests

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,8 +1,10 @@
+import axios from "axios"
 
+const axiosPublic=axios.create({
+    baseURL:"http://localhost:5000"
+})
 const useAxiosPublic = () => {
-  return (
-    <div>useAxiosPublic</div>
-  )
+    return axiosPublic
 }
 
 export default useAxiosPublic
